40dc22c23e8be15207189976d67062884121b31a
Mohammad Qabalany <qabalany@gmail.com>
2025-12-18 01:48:43 +0100

    Setup project with React Router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import MovieDetail from './pages/MovieDetail'
+
 export const App = () => {
   return (
-    <h1>Movies</h1>
+    // Routes container - only one route matches at a time
+    <Routes>
+      {/* Home page - shows at root URL "/" */}
+      <Route path="/" element={<Home />} />
+      
+      {/* Movie detail page - :id is a dynamic parameter */}
+      {/* Example: /movies/123 → id = "123" */}
+      <Route path="/movies/:id" element={<MovieDetail />} />
+    </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './App.jsx'
 
 import './index.css'
 
+// BrowserRouter wraps the entire app to enable routing
+// It uses the browser's History API to keep UI in sync with URL
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
